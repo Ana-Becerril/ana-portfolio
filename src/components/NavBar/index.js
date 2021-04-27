4aaab2645c951ae33d98baaea7ecc9da9985ea6a
Ana- Becerril <ana.becbel@gmail.com>
2021-04-27 13:00:50 -0500

    Added an icon to scroll and for the social links

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import styles from './style.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithubSquare, faLinkedin, faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
+
+
+
 
 const NavBar = () => (
 
@@ -9,6 +14,11 @@ const NavBar = () => (
             <div className= {styles.about}>ABOUT</div>
             <div className= {styles.projects}>PROJECTS</div>
             <div className= {styles.contact}>CONTACT</div>
+        </div>
+        <div className={styles.iconsBar}>
+            <FontAwesomeIcon icon ={faGithubSquare} className={styles.githubIcon}/>
+            <FontAwesomeIcon icon ={faLinkedin} className={styles.linkedinIcon}/>
+            <FontAwesomeIcon icon ={faTwitterSquare} className={styles.twitterIcon}/>
         </div>
     </div>
 );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import HomeMain from '../components/HomeMain'
 import NavBar from '../components/NavBar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronDown} from '@fortawesome/free-solid-svg-icons';
+import Animista, { AnimistaTypes } from 'react-animista'
 
 
 const Home = ()=>
@@ -11,7 +12,9 @@ const Home = ()=>
   <div className={styles.homeContainer}>
                <NavBar />
                <HomeMain />
+               <Animista iterationCount={300} duration="2s" type={AnimistaTypes.SLIDE_BCK_BOTTOM}>
                <FontAwesomeIcon icon ={faChevronDown} className={styles.arrowIcon}/>
+               </Animista>
          </div>
   
 );
