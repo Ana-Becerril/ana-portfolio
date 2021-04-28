6755c7b05896733aa07451d7516b1580498cb9c1
Ana- Becerril <ana.becbel@gmail.com>
2021-04-28 16:58:34 -0500

    added a title, chaged about me info, added a title and finished the about section

--- src/components/AboutRight/index.js
+++ src/components/AboutRight/index.js
@@ -7,6 +7,7 @@ import { faCircle } from '@fortawesome/free-solid-svg-icons'
 const AboutRight = () => (
 
     <div className={styles.parent}>
+        <div className={styles.title}>ABOUT ME</div>
         <div className={styles.console}>
             <div className={styles.header}>
                 <div className={styles.circlesContainer}>
@@ -16,9 +17,8 @@ const AboutRight = () => (
                 </div>
             </div>
             <div className={styles.body}>
-                <p> Hi my name is Ana. I enjoy beautiful and functional web design.
-Among my main technical skills are HTML / CSS / JS / UX/ Responsive Design / Photoshop.
- </p>
+                <p> 👋Hi, I'm Ana Becerril a Frontend Developer helping to make the digital world a pleasant and user-friendly place through the use of technologies 👩‍💻.  One of my great passions is making interfaces that are attractive and usable for anyone.  I learn easily (and I continue to do so), discipline and perseverance are values ​​that define me.  If you like what you see, contact me.⭐
+                </p>
             </div>
         </div>
     </div>
--- src/constants/particle.js
+++ src/constants/particle.js
@@ -77,7 +77,7 @@ const particles = {
         },
         "onclick": {
           "enable": true,
-          "mode": "push"
+          "mode": ""
         },
         "resize": true
       },
