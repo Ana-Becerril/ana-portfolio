13605d4e054259af229a70ea5230cc1c9c1ddb76
Ana- Becerril <ana.becbel@gmail.com>
2021-04-27 22:43:50 -0500

    styled the profile image and did a console for set info about me

--- src/components/AboutRight/index.js
+++ src/components/AboutRight/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import styles from './style.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons'
+
 
 const AboutRight = () => (
 
     <div className={styles.parent}>
-        
+        <div className={styles.console}>
+            <div className={styles.header}>
+                <div className={styles.circlesContainer}>
+                <FontAwesomeIcon icon ={faCircle} className={styles.redCircle}/>
+                <FontAwesomeIcon icon ={faCircle} className={styles.yellowCircle}/>
+                <FontAwesomeIcon icon ={faCircle} className={styles.greenCircle}/>
+                </div>
+            </div>
+            <div className={styles.body}>
+                <p> Hi my name is Ana. I enjoy beautiful and functional web design.
+Among my main technical skills are HTML / CSS / JS / UX/ Responsive Design / Photoshop.
+ </p>
+            </div>
+        </div>
     </div>
 
 );
