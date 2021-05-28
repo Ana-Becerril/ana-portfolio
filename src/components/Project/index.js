d9457fc1c824442910f2c3152e4f423680b6cea2
Ana- Becerril <ana.becbel@gmail.com>
2021-05-28 13:00:07 -0500

    changed the responsive design of projects page

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -5,32 +5,34 @@ const Project = ({ title, image, imageMobile, description, year, stacksList, isB
     return (
 
         <div style={{ backgroundColor: isBlack ? 'rgb(236, 182, 2)' : 'black' }} className={styles.parent}>
-            <div className={styles.mokupContainer}>
-                <img alt="mockup" src={image} className={styles.mokup} />
-                <img alt="mockup" src={imageMobile} className={styles.mokupMobile} />
-            </div>
-            <div className={styles.infoContainer}>
-                <div style={{ color: isBlack ? 'black' : 'white' }} className={styles.titleProject}>{title}</div>
-                <div style={{ color: isBlack ? 'black' : 'white' }} className={styles.year}>{year}</div>
-                <div style={{ color: isBlack ? 'black' : 'white' }} className={styles.infoProject}>{description}</div>
-                <div style={{ color: isBlack ? 'black' : 'white' }} className={styles.stackContainer}>
-                    <div style={{ color: isBlack ? 'black' : 'white' }} className={styles.stackTitle}>Stack:</div>
-                    <div className={styles.stackList}>
-                        {(stacksList && stacksList.length > 0) ? (
-                            <>
-                                {stacksList.map(item => {
-                                    return (
-                                        <div style={{ color: isBlack ? 'black' : 'white' }} className={styles.stack}>⭐{item.title}</div>
-                                    )
-                                })}
-                            </>
-                        ) : (<p>nothing here</p>)}
-                    </div>
+            <div style={{ color: isBlack ? 'black' : 'white' }} className={styles.titleProject}>{title}</div>
+            <div className={styles.mainContainer}>
+                <div className={styles.mokupContainer}>
+                    <img alt="mockup" src={image} className={styles.mokup} />
+                    <img alt="mockup" src={imageMobile} className={styles.mokupMobile} />
                 </div>
-                <div className={styles.buttonsContainer}>
-                <a href={linkRepo} style={{ color: isBlack ? 'black' : 'white' }} className= {`${styles.buttonGithub} ${styles.coolLink}`}>REPOSITORY</a>
+                <div className={styles.infoContainer}>
+                    <div style={{ color: isBlack ? 'black' : 'white' }} className={styles.year}>{year}</div>
+                    <div style={{ color: isBlack ? 'black' : 'white' }} className={styles.infoProject}>{description}</div>
+                    <div style={{ color: isBlack ? 'black' : 'white' }} className={styles.stackContainer}>
+                        <div style={{ color: isBlack ? 'black' : 'white' }} className={styles.stackTitle}>Stack:</div>
+                        <div className={styles.stackList}>
+                            {(stacksList && stacksList.length > 0) ? (
+                                <>
+                                    {stacksList.map(item => {
+                                        return (
+                                            <div style={{ color: isBlack ? 'black' : 'white' }} className={styles.stack}>⭐{item.title}</div>
+                                        )
+                                    })}
+                                </>
+                            ) : (<p>nothing here</p>)}
+                        </div>
+                    </div>
+                    <div className={styles.buttonsContainer}>
+                        <a href={linkRepo} style={{ color: isBlack ? 'black' : 'white' }} className={`${styles.buttonGithub} ${styles.coolLink}`}>REPOSITORY</a>
 
-                <a href={linkLive} style={{ color: isBlack ? 'black' : 'white' }} className={`${styles.buttonLive} ${styles.coolLink}`}>LIVE DEMO</a>
+                        <a href={linkLive} style={{ color: isBlack ? 'black' : 'white' }} className={`${styles.buttonLive} ${styles.coolLink}`}>LIVE DEMO</a>
+                    </div>
                 </div>
             </div>
         </div>
