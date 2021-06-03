ba357b3c51d4839d035d6976a73e17da53dbc164
Ana- Becerril <ana.becbel@gmail.com>
2021-06-02 22:40:39 -0500

    added a component that contains a modal to show a form for the mobile version

--- src/components/ContactRight/index.js
+++ src/components/ContactRight/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import styles from './style.module.css';
-
+import Modal from '../Modal'
 
 const ContactRight = () => (
     <>
-    <div className={styles.parent}>
-    
-    <form>
-    <input type="text" className={styles.fname} value="Name"></input>
-    <input type="text" className={styles.fmail} value="Email"></input>
-    <input type="text" className={styles.fbody} value=""></input>
-    </form>
-    <button className={styles.DVersion}>Get in touch</button>
-    <button className={styles.MVersion}>Contact Form</button>
-    </div>
+        <div className={styles.parent}>
+
+            <form>
+                <input type="text" className={styles.fname} value="Name"></input>
+                <input type="text" className={styles.fmail} value="Email"></input>
+                <input type="text" className={styles.fbody} value=""></input>
+            </form>
+            <button className={styles.DVersion}>Get in touch</button>
+            <button className={styles.MVersion}>Contact Form</button>
+            <Modal />
+        </div>
 
     </>
 );
---
+++ src/components/Modal/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './style.module.css';
+
+
+const Modal = () => {
+    return(
+        <div className={styles.modal}>
+            <div className={styles.modalContent}>
+            <div className={styles.parent}>
+
+<form>
+    <input type="text" className={styles.fname} value="Name"></input>
+    <input type="text" className={styles.fmail} value="Email"></input>
+    <input type="text" className={styles.fbody} value=""></input>
+</form>
+<button className={styles.DVersion}>Get in touch</button>
+</div>
+            </div>
+        </div>
+    )
+};
+
+export default Modal;
