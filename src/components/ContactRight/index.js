caee260e1736428edcb4b175c03918cbf2e0da30
Ana- Becerril <ana.becbel@gmail.com>
2021-06-09 23:30:24 -0500

    changed the values on the form to send emails

--- src/components/ContactRight/index.js
+++ src/components/ContactRight/index.js
@@ -12,10 +12,34 @@ const ContactRight = () => {
         <>
             <div className={styles.parent}>
 
-                <form>
-                    <input type="text" className={styles.fname} value="Name"></input>
-                    <input type="text" className={styles.fmail} value="Email"></input>
-                    <input type="text" className={styles.fbody} value=""></input>
+                <form onSubmit={onSubmit}>
+                    <input 
+                    type="text" 
+                    className={styles.fname} 
+                    name='from_name'
+                    placeholder='from name'
+                    value={toSend.from_name}
+                    onChange={handleChange}
+                    >
+                    </input>
+                    <input 
+                    type="text" 
+                    className={styles.fmail} 
+                    value="Email"
+                    name='to_name'
+                    placeholder='to name'
+                    value={toSend.to_name}
+                    onChange={handleChange}>
+                    </input>
+                    <input 
+                    type="text" 
+                    className={styles.fbody} 
+                    name='message'
+                    placeholder='Your message'
+                    value={toSend.message}
+                    onChange={handleChange}
+                    >
+                    </input>
                 </form>
                 <button className={styles.DVersion}>Get in touch</button>
                 <button onClick={() => setShow(true)} className={styles.MVersion}>Contact Form</button>
